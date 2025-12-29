14369523a5af321cce89ea697db35c34ee418565
Nicholas hillier <nicholas@ghostmonk.com>
2025-12-29 09:36:08 -0500

    Fix mobile navigation bar

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -11,7 +11,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     const isMobile = useIsMobile();
 
     return (
-        <div className="min-h-screen text-foreground dark:bg-gray-900 bg-white transition-colors duration-300">
+        <div className="min-h-dvh text-foreground dark:bg-gray-900 bg-white transition-colors duration-300">
             <TopNav />
             <main className={`container mx-auto p-6 ${isMobile ? 'pb-bottom-nav' : ''}`}>
                 {children}
